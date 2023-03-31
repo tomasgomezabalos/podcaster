98e251e9e7a76998f6d06041420ed36cbdd397eb
Tomas Gomez Abalos <tomas300472@gmail.com>
2023-03-31 15:58:11 +0200

    feat: adding cross-env for all environments

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import {defineConfig} from '@playwright/test';
 export default defineConfig({
   testDir: './tests/e2e',
   webServer: {
-    command: 'NODE_ENV=test BROWSER=none yarn dev',
+    command: 'cross-env NODE_ENV=test BROWSER=none yarn dev',
     port: 3000
   },
   reporter: 'list'
